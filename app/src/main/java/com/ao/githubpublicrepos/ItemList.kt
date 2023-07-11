cffa92db881ba7334544b08bf7df1334e35b616a
Angel Juarez <angeloj5@hotmail.com>
2023-07-10 22:42:45 -0600

    Creating new project to display github repos.

---
+++ app/src/main/java/com/ao/githubpublicrepos/ItemList.kt
@@ -0,0 +1,11 @@
+package com.ao.githubpublicrepos
+
+class ItemList {
+    var title: String? = null
+    var detail: String? = null
+
+    constructor(title: String?, detail:String?){
+        this.title = title
+        this.detail = detail
+    }
+}
